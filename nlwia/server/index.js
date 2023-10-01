15f56b938641ab37bed48555c0dd8e49dda31fa0
Renan Rodrigues <renan_ime_2012@hotmail.com>
2023-10-01 20:27:07 -0300

    transcrição de audio para texto

--- nlwia/server/index.js
+++ nlwia/server/index.js
@@ -12,18 +12,28 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/summary/:id", async (request,response)=>{
-    await download(request.params.id)
-    const audioConverted = await convert()
-    // console.log(audioConverted)
-    const result = await transcribe()
-    return response.json({ result })
+    try{
+        await download(request.params.id)
+        const audioConverted = await convert()
+        const result = await transcribe(audioConverted)
+        return response.json({ result })
+    }catch(error){
+        console.log(error)
+        return response.json({ error })
+    }
+    
 })
 
 app.post("/summary",async (request,response)=>{
-    console.log("teste indo")
-    const result = await summarize(request.body.text)
-    
-    return response.json({result})
+    try{
+        console.log("teste indo")
+        const result = await summarize(request.body.text)
+        
+        return response.json({result})
+    }catch(error){
+        console.log(error)
+        return response.json({ error })
+    }
 })
 
 app.listen(3333,()=> console.log("Server is running on port 3333 blz?"))
